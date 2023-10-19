c5bbd07afae11df5693eeb65bb4338b30ebfc0ed
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-10-19 20:06:34 -0300

    Projeto iniciado

--- backEnd/index.js
+++ backEnd/index.js
@@ -12,6 +12,20 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.post('/todos', async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            'INSERT INTO todo (description) VALUES($1) RETURNING *',
+            [description]
+        );
+
+        res.json(newTodo.rows[0]);
+} catch (err) {
+    console.log(err.message);
+}
+})
+
 app.listen(5000, () => {
     console.log('Server started on port 5000');
 })
