6c95295bbc7b43e6c7193bf2717f5be70c0956fe
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-10-20 12:43:27 -0300

    Projeto em desenvolvimento

--- backEnd/index.js
+++ backEnd/index.js
@@ -12,6 +12,27 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/todos/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [id]);
+        res.json(todo.rows[0]);
+    }catch(err){
+        console.log(err.message);
+    }
+})
+
+//?                  LISTAR TODOS OS ITENS
+
+app.get('/todos', async (req, res) => {
+    try{
+        const todo = await pool.query('SELECT * FROM todo');
+        res.json(todo.rows);
+    }catch(err){
+        console.log(err.message);
+    }
+})
+
 app.post('/todos', async (req, res) => {
     try {
         const { description } = req.body;
@@ -25,6 +46,19 @@ app.post('/todos', async (req, res) => {
     console.log(err.message);
 }
 })
+
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query(
+            'UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *',
+            [description, id]
+        );
+        }
+    catch (err) {
+        console.log(err.message);
+    }})   
 
 app.listen(5000, () => {
     console.log('Server started on port 5000');
